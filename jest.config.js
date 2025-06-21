84f6bde3d8934d2ea48b68079ec44c7bb767b399
Taketoshi Nishimori <nistake@gmail.com>
2025-06-21 16:06:43 +0900

    feat: ユニットテスト環境を構築し最初のテストを追加

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+export default {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+}; 
